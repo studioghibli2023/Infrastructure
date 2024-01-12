4b72aead245044317178cddc215a16a0a3959f39
L00179719 <L00179719@atu.ie>
2024-01-12 18:13:26 +0000

    create private route to NAT Gat

--- main.tf
+++ main.tf
@@ -126,6 +126,13 @@ resource "aws_route" "public_route" {
   gateway_id             = aws_internet_gateway.my_igw.id
 }
 
+
+resource "aws_route" "private_subnet_1_default_route" {
+  route_table_id         = aws_route_table.private_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.my_nat_gateway.id  # NAT Gateway ID
+}
+
 # Create Route Table Associations
 resource "aws_route_table_association" "public_association" {
   subnet_id      = aws_subnet.public_subnet.id

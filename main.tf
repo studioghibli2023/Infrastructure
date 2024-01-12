9826f8b77e97b071fb6761275b8ec96bb5cb2987
L00179719 <L00179719@atu.ie>
2024-01-12 13:26:13 +0000

    created ECS task definition with fargate

--- main.tf
+++ main.tf
@@ -104,6 +104,57 @@ resource "aws_ecs_cluster" "my_ecs_cluster" {
   name = "${var.environment_name}-ecs-cluster"
 }
 
+
+# Create ECS Task Execution Role
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "${var.environment_name}-ecs-execution-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+# Attach Policies to ECS Task Execution Role (adjust policies as needed)
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"  # Attach policies as needed
+  role       = aws_iam_role.ecs_execution_role.name
+}
+
+# ECS Task Definition
+resource "aws_ecs_task_definition" "my_task_definition" {
+  family                   = "${var.environment_name}-task-family"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  
+
+  container_definitions = jsonencode([
+    {
+      name  = "tbbt-container"
+      image = aws_ecr_repository.my_ecr_repo.repository_url
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        },
+      ]
+    },
+  ])
+}
+
 ##############################  Outputs  #########################################
 
 # Output ECR URI

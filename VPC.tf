126e0d5eee83c59297cb99325ec69980854cdb1a
L00179719 <L00179719@atu.ie>
2024-01-12 11:51:29 +0000

    VPC.tf file created

---
+++ VPC.tf
@@ -0,0 +1,81 @@
+# Define the environment name
+variable "environment_name" {
+  default = "TBBT"
+}
+
+# Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "${var.environment_name}-vpc"
+  }
+}
+
+# Create Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"  # Change this to your preferred availability zone
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.environment_name}-public-subnet"
+  }
+}
+
+# Create Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1a"  # Change this to your preferred availability zone
+  tags = {
+    Name = "${var.environment_name}-private-subnet"
+  }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "${var.environment_name}-igw"
+  }
+}
+
+# Create Elastic IP
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+# Create NAT Gateway
+resource "aws_nat_gateway" "my_nat_gateway" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet.id
+  tags = {
+    Name = "${var.environment_name}-nat-gateway"
+  }
+}
+
+# Create Route Tables
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "${var.environment_name}-public-route-table"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "${var.environment_name}-private-route-table"
+  }
+}
+
+# Create Route Table Associations
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}

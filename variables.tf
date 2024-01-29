ddaea48bfa23ea3b6bf4fa80d82e1b9530b24e48
Adrian Iosif <146378579+L00179719@users.noreply.github.com>
2024-01-29 20:20:48 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "vpc_cidr_block" {
   default     = "10.0.0.0/16"
 }
 
-<<<<<<< HEAD
+
 variable "container_name" {
   description = "Define container name"
   type        = string
@@ -45,5 +45,3 @@ variable "db_password" {
   description = "RDS database password"
   default     = "StudioGhibli2023"
 }
-=======
->>>>>>> main

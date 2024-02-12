48bdcce3fc33e40c51cb6b9abe2aa5ae51259ef0
L00179719 <L00179719@atu.ie>
2024-02-12 21:18:58 +0000

    s3 and dynamodb for terraform

--- main.tf
+++ main.tf
@@ -171,10 +171,10 @@ resource "aws_route_table_association" "private_association_2" {
 
 terraform {
   backend "s3" {
-    bucket        = aws_dynamodb_table.tf_lock_table-new.arn
+    bucket        = aws_dynamodb_table.tf_lock_table-new.bucket
     key           = "my-environment/terraform.tfstate"
     region        = "us-east-1"
-    dynamodb_table = aws_s3_bucket.terraform_remote_state_file_new.arn
+    dynamodb_table = aws_s3_bucket.terraform_remote_state_file_new.name
     encrypt       = true
   }
 }

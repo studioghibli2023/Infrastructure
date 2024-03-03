4e4a98ec6fa8bb5d16f2b6866707eabf67c2d433
L00179719 <L00179719@atu.ie>
2024-03-03 17:42:49 +0000

    fargate memory

--- backend_fargate.tf
+++ backend_fargate.tf
@@ -86,7 +86,7 @@ resource "aws_ecs_task_definition" "backend_task_definition" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
-  memory                   = "2048"
+  memory                   = "3072"
 
   # ECS Container
   container_definitions = jsonencode([

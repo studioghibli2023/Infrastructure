5852ce884b154cea6acaac7d00a4ec4aa838bbea
Adrian Iosif <146378579+L00179719@users.noreply.github.com>
2024-03-26 22:21:17 +0000

    Update database.tf

--- database.tf
+++ database.tf
@@ -57,8 +57,8 @@ resource "aws_db_instance" "my_db_instance" {
   allocated_storage       = 20
   storage_type            = "gp2"
   engine                  = "mysql"
-  engine_version          = "8.0.36"
-  instance_class          = "db.t2.micro"
+  engine_version          = "5.7"
+  instance_class          = "db.t3.micro"
   db_name                 = var.db_name
   username                = var.db_username
   password                = var.db_password
